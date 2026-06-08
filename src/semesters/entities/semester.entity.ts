f477c50a190a0c7e82ff1a462ca2f678f7b86e37
nikanika22 <dinhhuyteam@gmail.com>
2026-06-08 10:20:20 +0700

    feat : Entity for Semesters

--- src/semesters/entities/semester.entity.ts
+++ src/semesters/entities/semester.entity.ts
@@ -1,1 +1,18 @@
-export class Semester {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('semesters')
+export class Semester {
+  @PrimaryColumn()
+  semester_id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  start_date: Date;
+
+  @Column()
+  end_date: Date;
+  @Column()
+  is_active: boolean;
+}
